a1fcd26486e74744d4bf2a040d4376fd126b7af0
Tanmai H <vvijaytanmai@gmail.com>
2020-06-18 10:11:49 +0530

    switched to cv2, preprocessing also shown

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import uuid
 from flask import Flask, render_template, redirect, url_for, request, flash
@@ -7,8 +6,8 @@ from werkzeug.utils import secure_filename as sfn
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage.io as io
-import skimage.transform as transform
+import cv2
+
 '''
 	Import Model & tf Here
 '''
@@ -28,7 +27,7 @@ app = Flask(__name__)
 '''
 	Load the weghts & make graph global
 '''
-# md.load_weights("model.h5")
+md.load_weights("model.h5")
 global graph
 graph = tf.compat.v1.get_default_graph()
 model=nasnet_model(15)
@@ -74,29 +73,32 @@ def upload():
 @app.route('/result', methods=['GET'])
 def result():
     uploaded_img = os.path.join('static', UPLOAD_FOLDER, request.args['uploaded_img'])
-    preprocessed_path = os.path.join('static', UPLOAD_FOLDER, 'preprocessed.jpg')
+    preprocessed_path = os.path.join('static', RESULT_FOLDER, request.args['uploaded_img'])
     app.logger.debug(uploaded_img)
-    img = transform.resize(io.imread(uploaded_img), INPUT_SHAPE)
     
+    img = cv2.resize(cv2.imread(uploaded_img, cv2.CV_8UC1), INPUT_SHAPE)
     app.logger.debug(img.shape)
-    fin_img=preprocess_image_filtered(img)
-    fin_img = np.expand_dims(fin_img, 0)    #Resize to (1, img_dim)
     
-
-
+    preprocessed = preprocess_image(img)
+    cv2.imwrite(preprocessed_path, preprocessed)
+    
+    fin_img = np.expand_dims(preprocessed, 0)    #Resize to (1, img_dim)
+    
     '''
         Model Prediction
     '''
     app.logger.debug(fin_img.shape)
-    # fin_img=np.reshape(fin_img,(1,331,331,3))
+    fin_img=np.reshape(fin_img,(1,331,331,3))
+    
     with graph.as_default():
         prediction_array = model.predict(fin_img)[0]
         app.logger.debug(prediction_array)
     
     prediction = predictions_to_type(prediction_array)
-
+    
     return render_template('result.html', 
                             uploaded_img=uploaded_img, 
+                            preprocessed_img=preprocessed_path,
                             result=prediction)
 
 if __name__ == "__main__":
